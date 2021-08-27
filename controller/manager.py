2c25360a030b54cc7f31e3c2108b5407e54c1987
TristanBoissy <65125057+TristanBoissy@users.noreply.github.com>
2021-08-27 18:37:49 -0400

    Add files via upload
    Changed the algorithm of infectious circles that infects not infecterd circles. Instead of taking an infected circle on the map and iterating through all the not infected circles and calculation the distance between the 2 to know if he is close or not. The new algorithm creates a 2d array the size of the map with a 0 or 1 as a value. Each grid represent a pixel and if the pixel is 0, it is not contagious, if it is 1, it is contagious. We update this map by creating an area with the shape of a circle with the radius predetermined by the user and we place these value in the 2d array. This area is now full of 1 and it has the shape of a circle. Then we iterate through all the not infected circles and check if their position is on a 0 or a 1 in the new array. If he is, then we calculate the probability of getting infected and update the circles attributes.
    
    I also added a new personnalize schedule that takes the time in miliseconds and play a turn with the timer decided in the constant TIMER
    0.05 ms seems to be the sweet spot for 200 circles. Might need to get the Timer higher to get more circles in the canvas and not have a sloppy animation

--- controller/manager.py
+++ controller/manager.py
@@ -1,29 +1,31 @@
 from constants import constants, csv_data_file
-from objects import circle_object
 from objects import position
 import random
-import time, threading
+import time
 import math
 from controller import get_value_from_entry_list
-import csv
+import numpy
 
 global instance
 
-class controller:
 
-    TIMER = 0.1
+class Controller:
+
+    TIMER = 0.05
 
     NOT_INFECTED_STATE = "not_infected"
     INFECTED_STATE = "infected"
     IMMUNE_STATE = "immune"
 
     map_list = []
-    infection_list = []
+    infected_list = []
 
     global graph_view
 
     def __init__(self, maps, entry):
         self.map_list = maps
+
+        self.infectious_pixels_list = numpy.zeros(shape=(self.map_list[0].get_width(),self.map_list[0].get_height()))
 
         self.max_circles = int(get_value_from_entry_list.get_value(entry, constants.MAX_CIRCLES_STRING))
 
@@ -36,7 +38,7 @@ class controller:
         self.chance_of_changing_a_direction = float(get_value_from_entry_list.get_value(
             entry, constants.CHANCE_OF_CHANGING_DIRECTION_STRING))
 
-        self.chance_of_changing_the_speed= float(get_value_from_entry_list.get_value(
+        self.chance_of_changing_the_speed = float(get_value_from_entry_list.get_value(
             entry, constants.CHANCE_OF_CHANGING_SPEED_STRING))
 
         self.number_of_infected_at_start = int(get_value_from_entry_list.get_value(
@@ -64,38 +66,49 @@ class controller:
 
         self.instance = self
 
-
     def get_instance(self):
         return self
-
 
     def start_game(self, graph_view):
         self.graph_view = graph_view
         self.add_circles_to_map()
-        self.play_game()
+        self.timer_task()
+
+
+    def timer_task(self):
+            self.play_game()
+            old_time = self.get_time_in_miliseconds()
+
+            while True:
+                if self.time_remaining > 0:
+                    new_time = self.get_time_in_miliseconds()
+                    if old_time+self.TIMER*1000 < new_time:
+                        old_time = new_time
+                        self.play_game()
+                else:
+                    break
+                time.sleep(0.001)
+
+    def get_time_in_miliseconds(self):
+        return round(time.time()*1000)
 
 
     def play_game(self):
-        while True:
-            time.sleep(self.TIMER)
-            self.play_circles_turn()
-            self.time_remaining -= self.TIMER
-            #self.update_graph()
-            if(self.check_time()):
-                break
+        self.play_circles_turn()
+        self.time_remaining -= self.TIMER
 
 
     def check_time(self):
         return math.ceil(self.time_remaining) == 0
 
-
     def infection_at_start(self, map):
         for i in range(self.number_of_infected_at_start):
+            circle = map.get_circle_from_list(self.random_int(0, self.max_circles - 1))
             self.change_circle_state(map,
-                                     map.get_circle_from_list(self.random_int(0, self.max_circles-1)),
+                                     circle,
                                      self.INFECTED_STATE,
                                      constants.INFECTED_COLOR)
-
+            self.infected_list.append(circle)
 
     def add_circles_to_map(self):
         for i in range(0, self.max_circles):
@@ -107,46 +120,48 @@ class controller:
         self.infection_at_start(self.map_list[0])
         self.map_list[0].update_canvas()
 
-
     def play_circles_turn(self):
         self.reset_infection_info()
+
         for i in self.map_list:
             circles_list = i.get_circles_list()
+
             for y in circles_list:
                 self.move_and_verify_circle_movement(i, y)
-                self.update_infection_info(y)
-                if(y.get_infection_state() == self.INFECTED_STATE):
-                    if(y.get_infection_time() != y.get_max_infection_time()):
+
+                if y.get_infection_state() == self.INFECTED_STATE:
+                    if y.get_infection_time() != y.get_max_infection_time():
                         y.set_infection_time(y.get_infection_time() + 1)
                     else:
                         self.change_circle_state(i, y, self.IMMUNE_STATE, constants.IMMUNE_COLOR)
+                        self.infected_list.remove(y)
+                self.update_infection_info(y)
 
+            self.reset_infection_pixels_list(self.map_list[0])
+            self.update_infectious_pixels()
             self.verify_infection(i)
+
             i.update_canvas()
+
         self.update_csv_file_data()
         self.update_graph_view()
 
-
     def circle_motion(self, circle):
-
-        if(self.check_if_first_circle_movement(circle)): # sets the value for the first turn
+        if self.check_if_first_circle_movement(circle):  # sets the value for the first turn
             self.play_first_circle_movement(circle)
             return
 
-        if(self.chance_of_changing_direction()):
-            self.change_circle_x_direction(circle) # changes x direction
+        if self.chance_of_changing_direction():
+            self.change_circle_x_direction(circle)  # changes x direction
 
-        if (self.chance_of_changing_direction()):
+        if self.chance_of_changing_direction():
             self.change_circle_y_direction(circle)  # changes y direction
 
-        if (self.chance_of_changing_speed()):
-            self.change_random_circle_x_speed(circle) # changes x speed
+        if self.chance_of_changing_speed():
+            self.change_random_circle_x_speed(circle)  # changes x speed
 
-        if (self.chance_of_changing_speed()):
-            self.change_random_circle_y_speed(circle) # changes y speed
-
-
-
+        if self.chance_of_changing_speed():
+            self.change_random_circle_y_speed(circle)  # changes y speed
 
     def play_first_circle_movement(self, circle):
         circle.set_x_direction(self.random_direction())
@@ -154,94 +169,136 @@ class controller:
         circle.set_x_speed(self.random_int(self.min_speed, self.max_speed))
         circle.set_y_speed(self.random_int(self.min_speed, self.max_speed))
 
-
     def check_if_first_circle_movement(self, circle):
-        return (circle.get_x_direction() == 0)
-
+        return circle.get_x_direction() == 0
 
     def chance_of_changing_direction(self):
-        return (self.random_float() < self.chance_of_changing_a_direction)
+        return self.random_float() < self.chance_of_changing_a_direction
 
-
-    def change_circle_x_direction(self,circle):
-        if(circle.get_x_direction() == 1):
+    def change_circle_x_direction(self, circle):
+        if circle.get_x_direction() == 1:
             circle.set_x_direction(-1)
         else:
             circle.set_x_direction(1)
 
-
     def change_circle_y_direction(self, circle):
-        if (circle.get_y_direction() == 1):
+        if circle.get_y_direction() == 1:
             circle.set_y_direction(-1)
         else:
             circle.set_y_direction(1)
 
-
     def chance_of_changing_speed(self):
-        return (self.random_float() < self.chance_of_changing_the_speed)
-
+        return self.random_float() < self.chance_of_changing_the_speed
 
     def change_random_circle_x_speed(self, circle):
         circle.set_x_speed(self.random_int(self.min_speed, self.max_speed))
 
-
     def change_random_circle_y_speed(self, circle):
         circle.set_y_speed(self.random_int(self.min_speed, self.max_speed))
 
-
     def random_position(self):
-        return position.position_2D(self.random_int(0,self.map_list[0].get_x_dimension()), # x position
-                                    self.random_int(0,self.map_list[0].get_y_dimension())) # y position
-
+        return position.position_2D(self.random_int(0, self.map_list[0].get_width()),  # x position
+                                    self.random_int(0, self.map_list[0].get_height()))  # y position
 
     def random_int(self, min, max):
         return random.randint(min, max)
 
-
     def random_float(self):
-        return random.uniform(0,1)
-
+        return random.uniform(0, 1)
 
     def random_direction(self):
-        rand = random.randint(0,1)
+        rand = random.randint(0, 1)
         if rand == 0:
             return -1
         else:
             return 1
 
-
     def move_and_verify_circle_movement(self, map, circle):
-
         self.circle_motion(circle)
         new_x_value = self.verify_circle_one_axis_position(circle.get_x() + circle.get_x_movement(),
-                                                                 map.get_x_dimension())
+                                                           map.get_width())
         new_y_value = self.verify_circle_one_axis_position(circle.get_y() + circle.get_y_movement(),
-                                                                 map.get_y_dimension())
+                                                           map.get_height())
         circle.set_x(new_x_value)
         circle.set_y(new_y_value)
         map.move_circle_on_map(circle)
 
-
     def verify_circle_one_axis_position(self, value, boundary):
-        if (value < 0):
+        if value < 0:
             return value + boundary
-        elif (value > boundary):
+        elif value > boundary:
             return value % boundary
         else:
             return value
-
 
     def change_circle_state(self, map, circle, infection_state, state_color):
         circle.set_infection_state(infection_state)
         circle.set_color(state_color)
         map.change_circle_color(circle, circle.get_color())
+        self.update_infectious_pixels()
 
+    def reset_infection_pixels_list(self,map):
+        self.infectious_pixels_list = numpy.zeros(shape=(map.get_width(),map.get_height()))
+        #for i in range(0, self.map_list[0].get_width-1):
+            #for j in range(0, self.map_list[0].get_height-1):
+               # self.infectious_pixels_list
+
+    def update_infectious_pixels(self):
+        for i in self.infected_list:
+
+            r = self.max_distance_for_infection
+            x = i.get_x()
+            y = i.get_y()
+            p = 0
+            q = r
+            d = 3 - 2*r
+
+            self.update_pixel(x, y, p, q, 1)
+
+            while (p <= q):
+                p += 1
+
+                if d > 0:
+                    d = d + (4 * (p - q)) + 10
+                    q -= 1
+                else:
+                    d = d + (4 * p) + 6
+
+                self.update_pixel(x, y, p, q, 1)
+
+
+    def update_pixel(self, x, y, p, q, new_value):
+        self.update_pixel_infected_line_x_axis((x - p), (x + p), (y + q), new_value)
+        self.update_pixel_infected_line_x_axis((x - p), (x + p), (y - q), new_value)
+        self.update_pixel_infected_line_x_axis((x - q), (x + q), (y + p), new_value)
+        self.update_pixel_infected_line_x_axis((x - q), (x + q), (y - p), new_value)
+
+    def update_pixel_infected_line_x_axis(self, x_min, x_max, y, new_value):
+        if y <= self.map_list[0].get_height() and y >= 0:
+            for i in range(int(x_min),int(x_max+1)):
+                if i <= self.map_list[0].get_width() and i >=0 :
+                    self.infectious_pixels_list[int(i-1)][int(y-1)] = int(new_value)
 
     def verify_infection(self, map):
         for i in map.get_circles_list():
-            if(i.get_infection_state() == self.INFECTED_STATE):
-                self.check_if_other_circle_is_close(map, i)
+            if i.get_infection_state() == self.NOT_INFECTED_STATE:
+                if(self.verify_if_on_infectious_pixel(i)):
+                    if self.chance_of_getting_infected():
+                        self.change_circle_state(map, i, self.INFECTED_STATE, constants.INFECTED_COLOR)
+                        self.infected_list.append(i)
 
+
+    def verify_if_on_infectious_pixel(self, circle):
+        x = int(circle.get_x())
+        y = int(circle.get_y())
+        if (self.infectious_pixels_list[x-1][y-1] == 1):
+                return True
+        return False
+
+    #def verify_infection(self, map):
+    #    for i in map.get_circles_list():
+    #        if i.get_infection_state() == self.INFECTED_STATE:
+    #            self.check_if_other_circle_is_close(map, i)
 
     def check_if_other_circle_is_close(self, map, circle):
 
@@ -250,22 +307,18 @@ class controller:
 
         for i in map.get_circles_list():
             if i is not circle and i.get_infection_state() == self.NOT_INFECTED_STATE:
-                    if(self.check_if_distance_is_close(self.calculate_distance(x, y, i.get_x(), i.get_y()))):
-                        if(self.chance_of_getting_infected()):
-                            self.change_circle_state(map, i, self.INFECTED_STATE, constants.INFECTED_COLOR)
-
+                if self.check_if_distance_is_close(self.calculate_distance(x, y, i.get_x(), i.get_y())):
+                    if self.chance_of_getting_infected():
+                        self.change_circle_state(map, i, self.INFECTED_STATE, constants.INFECTED_COLOR)
 
     def chance_of_getting_infected(self):
         return self.random_float() < self.infection_probability
 
-
     def check_if_distance_is_close(self, distance):
         return distance <= self.max_distance_for_infection
 
-
     def calculate_distance(self, x1, y1, x2, y2):
-        return (math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2)))
-
+        return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
     def update_csv_file_data(self):
         csv_data_file.update_csv_file(self.get_duration(),
@@ -273,21 +326,18 @@ class controller:
                                       self.infected_circles,
                                       self.immune_circles)
 
-
     def reset_infection_info(self):
         self.not_infected_circles = 0
         self.infected_circles = 0
         self.immune_circles = 0
 
-
     def update_infection_info(self, circle):
-        if(circle.get_infection_state() == self.NOT_INFECTED_STATE):
+        if circle.get_infection_state() == self.NOT_INFECTED_STATE:
             self.not_infected_circles += 1
-        if(circle.get_infection_state() == self.INFECTED_STATE):
+        if circle.get_infection_state() == self.INFECTED_STATE:
             self.infected_circles += 1
-        if(circle.get_infection_state() == self.IMMUNE_STATE):
+        if circle.get_infection_state() == self.IMMUNE_STATE:
             self.immune_circles += 1
-
 
     def update_graph_view(self):
         self.graph_view.update_graph_data()
